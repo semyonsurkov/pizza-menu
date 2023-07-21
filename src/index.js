41e341831d72bf40c9bb1824b77cdf06737b268f
semyonsurkov <semyonsurkov@outlook.com>
2023-07-21 17:29:49 +0300

    Add new logic for open hours

--- src/index.js
+++ src/index.js
@@ -73,9 +73,20 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  // else alert("Sorry we're closes");
+
   return (
     <footer>{new Date().toLocaleTimeString()}.We're currently open!</footer>
   );
+
+  //return React.createElement("footer", null, "We're currently open!");
 }
 
 function Pizza() {
